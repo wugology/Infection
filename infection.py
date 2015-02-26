7b9d0f2def38107f3091f3bde52b0c372e4dbf4e
wugology <onna.nelson@gmail.com>
2015-02-26 13:39:36 -0800

    update to total_infection()
    more efficient version

--- infection.py
+++ infection.py
@@ -14,7 +14,7 @@ def getUserList():
         userList.append(coachList[n].name) #adds coach's name
         for s in range(0,len(coachList[n].students)):
             userList.append(coachList[n].students[s]) #adds students' names
-    userList = numpy.unique(userList)
+    userList = numpy.unique(userList) #get rid of duplicates
     return userList
 
 
@@ -23,16 +23,13 @@ def total_infection(user):
     for n in range(0,len(coachList)): #outer loop ensures double checking
         for m in range(0,len(coachList)): #check all the coaches
             if coachList[m].name in infected: #if a coach is infected
-                for i in range(0,len(coachList[m].students)): #check all the coach's students
-                    if coachList[m].students[i] not in infected: #if the student isn't infected
-                        infected.append(coachList[m].students[i]) #then the student is added to the list
+                infected.extend(coachList[m].students) #then all the students are added to the list
             for s in range(0,len(coachList[m].students)): #check all the coach's students
                 if coachList[m].students[s] in infected: #if the student is infected
                     if coachList[m].name not in infected: #if the coach isn't already infected
                         infected.append(coachList[m].name) #then the coach is added to the list
-                        for j in range(0,len(coachList[m].students)): #check all the coach's students
-                            if coachList[m].students[j] not in infected: #if the student isn't infected
-                                infected.append(coachList[m].students[j]) #infect the student
+                        infected.extend(coachList[m].students) #then all the coach's students are infected
+    infected = numpy.unique(infected) #get rid of duplicates
     return infected
 
 
